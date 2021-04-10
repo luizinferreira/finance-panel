ec915eb223d3b468c68c32483020c625740fe0e3
Luiz <luizeduardo_2904@live.com>
2021-04-10 10:26:46 -0400

    Correcao erro modal

--- scripts.js
+++ scripts.js
@@ -1,13 +1,3 @@
-const Modal = {
-    open(){
-        document.querySelector('.modal').classList.add('active')
-    },
-
-    close(){
-        document.querySelector('.modal').classList.remove('active')
-    }
-}
-
 const transactions = [
     {
         id= 1,
